1265579f8fcf38e4e5912f69d6b03f5be251d270
Hugo Valent <valenthugo@hotmail.com>
2022-07-03 15:43:52 +0100

    add state gdp per capita

--- ml_model/preprocess.py
+++ ml_model/preprocess.py
@@ -8,3 +8,9 @@ df.head()
 # do we have one row per customer 
 df["Customer"].unique().shape[0] == df["Customer"].shape[0]
 # %%
+gdp_df = pd.read_csv("../gdp-perstate-wikipedia.csv")
+# %%
+gdp_df['state'] = gdp_df['state'].apply(lambda x: x.rstrip("*"))
+# %%
+gdp_df['gdp_per_capita'] = gdp_df['gdp_per_capita'].apply(lambda x: x.lstrip("$").replace(",", ""))
+# %%
