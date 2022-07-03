dcd89071b44aaf6bcf54f212af63bb410e2c6123
Hugo Valent <valenthugo@hotmail.com>
2022-07-03 20:57:13 +0100

    define endpoints

--- controllers/app.py
+++ controllers/app.py
@@ -3,6 +3,11 @@ from ml_model.train import training_function
 from ml_model.predict import predict_clv
 from ml_model.preprocess import load_and_preprocess
 
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
 predictive_columns = [
         "state_gdp_percapita",
         "Coverage", 
@@ -11,6 +16,7 @@ predictive_columns = [
 
 target_column = "clv"
 
+@app.route("/train_model")
 def train_model_endpoint(request):
     train_filepaths = request["train_filepaths"]
     gdp_filepath = request["gdp_filepath"]
@@ -25,12 +31,13 @@ def train_model_endpoint(request):
     
     return is_model, 200 if is_model else 500
 
-def predict_clv_endpoint(request):
-    customer_id = request["customer_id"]
-    customer_data = request["data"]
+@app.route("/predict_clv")
+def predict_clv_endpoint():
+    customer_id = request.json["customer_id"]
+    customer_data = request.json["data"]
 
-    model_path = "../model.jbl"
-    clv_prediction = predict_clv(customer_data, model_path)
+    model_path = "model.jbl"
+    clv_prediction = predict_clv([customer_data], model_path)
 
     return {
         "customer_id": customer_id,
