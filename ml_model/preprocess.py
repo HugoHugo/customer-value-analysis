3fa25121dfebe0b8d7a3897ac391355524ad0e17
Hugo Valent <valenthugo@hotmail.com>
2022-07-03 13:13:35 +0100

    check data

--- ml_model/preprocess.py
+++ ml_model/preprocess.py
@@ -3,3 +3,8 @@ import pandas as pd
 # %%
 df = pd.read_csv("../WA_Fn-UseC_-Marketing-Customer-Value-Analysis.csv")
 # %%
+df.head()
+# %%
+# do we have one row per customer 
+df["Customer"].unique().shape[0] == df["Customer"].shape[0]
+# %%
